29f5800ae1c94927a06350fdac12b9af869b2ee0
CRAXCAT <70342707+CRAXCAT@users.noreply.github.com>
2026-04-14 17:02:19 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -5,7 +5,7 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://wjfbegtelplvyarrsjst.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_VNucvhVZoKkLx-UnZ0P7dw_y8c0ve-q';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqZmJlZ3RlbHBsdnlhcnJzanN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNzgyNzQsImV4cCI6MjA5MTc1NDI3NH0.dNVWyk_-Qj-N68dh59Xh3nMeVddXpOBumQM3pkLEH-E';
 
 // Inicializar cliente Supabase
 const { createClient } = supabase;
