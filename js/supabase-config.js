f102de22b40c3a903ebd88e1ac8129243aeeed63
CRAXCAT <70342707+CRAXCAT@users.noreply.github.com>
2026-04-21 08:24:50 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,8 +4,8 @@
 //  Dashboard → Settings → API
 // ============================================================
 
-const SUPABASE_URL = 'https://TU-PROJECT-ID.supabase.co';
-const SUPABASE_ANON_KEY = 'TU-ANON-KEY-AQUI';
+const SUPABASE_URL = 'https://wjfbegtelplvyarrsjst.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqZmJlZ3RlbHBsdnlhcnJzanN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNzgyNzQsImV4cCI6MjA5MTc1NDI3NH0.dNVWyk_-Qj-N68dh59Xh3nMeVddXpOBumQM3pkLEH-E';
 
 // Inicializar cliente Supabase
 const { createClient } = supabase;
